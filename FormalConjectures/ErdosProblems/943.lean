f3d6cde94b058b8a9bdf1cd548c247a08e51bab0
Salvatore Mercuri <47568553+smmercuri@users.noreply.github.com>
2026-02-18 07:18:53 +0000

    fix(ErdosProblems): 943 (#2332)
    The `=` should be an `≤` because "eventually zero" should be a
    possibility. See also [Thomas Bloom's
    comment](https://www.erdosproblems.com/forum/thread/943#post-2158)

--- FormalConjectures/ErdosProblems/943.lean
+++ FormalConjectures/ErdosProblems/943.lean
@@ -31,8 +31,7 @@ Let $A$ be the set of powerful numbers. Is is true that $1_A\ast 1_A(n)=n^{o(1)}
 -/
 @[category research open, AMS 11]
 theorem erdos_943 : answer(sorry) ↔
-    ∃ (o : ℕ → ℝ), o =o[atTop] (1 : ℕ → ℝ) ∧ ∀ᶠ n in atTop, (sumRep Powerful n) = (n : ℝ)^(o n)
-    := by
+    ∃ (o : ℕ → ℝ), o =o[atTop] (1 : ℕ → ℝ) ∧ ∀ᶠ n in atTop, (sumRep Powerful n) ≤ (n : ℝ)^(o n) := by
   sorry
 
 end Erdos943
